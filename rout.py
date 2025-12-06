c392cfde494ce5b8acf621b3d8521d46c5304d65
unknown <sajanneyveli@gmail.com>
2025-12-07 04:37:08 +0530

    .

--- rout.py
+++
@@ -1,7 +0,0 @@
-
-import importlib, sys
-m = importlib.import_module('server')
-print("Loaded file:", m.__file__)
-print("Routes:")
-for rule in m.app.url_map.iter_rules():
-    print(f"{rule}  -> methods={sorted(rule.methods)}")
